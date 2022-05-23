fdabba42e0ecc19fad155df494e1655dd1b81aaf
iamasteriix <59852540+iamasteriix@users.noreply.github.com>
2022-05-23 01:25:05 -0700

    split up tests

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -27,40 +27,47 @@ const variables_2 = { input: { email: 'em_com', password: password } };
 const variables_3 = { input: { email: email, password: 'long' } };
 const variables_4 = { input: { email: 'email', password: 'pass' } };
 
-it('Registering users test', async () => {
-    const response_0 = await request(getAddress(), mutation, variables_1);
-    const response_1 = await request(getAddress(), mutation, variables_1);
-    const response_2 = await request(getAddress(), mutation, variables_2);
-    const response_3 = await request(getAddress(), mutation, variables_3);
-    const response_4 = await request(getAddress(), mutation, variables_4);
+describe('Registering users test', () => {
+    it("Test endpoint and user registration data", async () => {
+        const response_0 = await request(getAddress(), mutation, variables_1);
 
-    // test endpoint data
-    expect(response_0).toBeTruthy();
-    expect(response_0.errors).toBeFalsy();
-    expect(response_0).toHaveProperty('register');
-    expect(response_0.register).toBe(null);
+        // test endpoint data
+        expect(response_0).toBeTruthy();
+        expect(response_0.errors).toBeFalsy();
+        expect(response_0).toHaveProperty('register');
+        expect(response_0.register).toBe(null);
 
-    // test user registration input data
-    const users = await User.find({ where: { email } });
-    const user = users[0];
-    expect(users).toHaveLength(1);
-    expect(user.email).toEqual(email);
-    expect(user.password).not.toEqual(password);
+        // test user registration input data
+        const users = await User.find({ where: { email } });
+        const user = users[0];
+        expect(users).toHaveLength(1);
+        expect(user.email).toEqual(email);
+        expect(user.password).not.toEqual(password);
+    });
 
-    // test for duplicate emails
-    expect(response_1.register).toHaveLength(1);
-    expect(response_1.register[0]).toEqual({ path: 'email', message: errorMessages.duplicateEmail });
+    it("Check for duplicate emails", async () => {
+        const response_1 = await request(getAddress(), mutation, variables_1);
+        expect(response_1.register).toHaveLength(1);
+        expect(response_1.register[0]).toEqual({ path: 'email', message: errorMessages.duplicateEmail });
+    });
+    
+    it("Check for bad email", async () => {
+        const response_2 = await request(getAddress(), mutation, variables_2);
+        expect(response_2.register).toHaveLength(1);
+        expect(response_2.register[0]).toEqual({ path: 'email', message: errorMessages.badEmail });
+    });
 
-    // test for bad email
-    expect(response_2.register).toHaveLength(1);
-    expect(response_2.register[0]).toEqual({ path: 'email', message: errorMessages.badEmail });
+    it("Check for short password", async () => {
+        const response_3 = await request(getAddress(), mutation, variables_3);
+        expect(response_3.register).toHaveLength(1);
+        expect(response_3.register[0]).toEqual({ path: 'password', message: errorMessages.shortPassword });
+    });
 
-    // test for short password
-    expect(response_3.register).toHaveLength(1);
-    expect(response_3.register[0]).toEqual({ path: 'password', message: errorMessages.shortPassword });
-
-    // test for bad email and short password
-    expect(response_4.register).toHaveLength(2);
+    it("Check for bad email and short password", async () => {
+        const response_4 = await request(getAddress(), mutation, variables_4);
+        expect(response_4.register).toHaveLength(2);
+    });
+    
 });
 
 // TODO: close whatever connection exists after jest test.
